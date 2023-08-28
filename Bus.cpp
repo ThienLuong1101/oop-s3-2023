76b39113f37215cfa891dc59d68e608bf3902141
luongthien <luongthien1236@gmail.com>
2023-08-28 12:09:32 +0930

    check

--- Bus.cpp
+++ Bus.cpp
@@ -5,6 +5,6 @@ Bus::Bus(int id) {};
 int Bus::getParkingDuration() {
     time_t currTime = time(nullptr);
     int elapsedTime = static_cast<int>(currTime - getTime());
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000 - 750));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     return elapsedTime; 
 }
